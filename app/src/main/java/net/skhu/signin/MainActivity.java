51154d9d413aa1920b76e0f1427fe7fba19ba50d
HHyeseon <contedete15@gmail.com>
2019-09-16 22:24:21 +0900

    SignInUpWithoutGoogle

--- app/src/main/java/net/skhu/signin/MainActivity.java
+++ app/src/main/java/net/skhu/signin/MainActivity.java
@@ -1,5 +1,6 @@
 package net.skhu.signin;
 
+<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,12 +24,37 @@ public class MainActivity extends AppCompatActivity {
     static final int RC_SIGN_IN = 337;
     TextView textView;
 
+=======
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private Button join;
+    private Button login;
+    private EditText email_login;
+    private EditText pwd_login;
+    FirebaseAuth firebaseAuth;
+>>>>>>> SignInUpWithoutGoogle
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< HEAD
         textView = (TextView) findViewById(R.id.textView);
         Button btnSignIn = findViewById(R.id.btn_signIn);
         Button btnSignOut = findViewById(R.id.btn_signOut);
@@ -93,6 +119,34 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
             }
         }
+=======
+        login = (Button) findViewById(R.id.main_login_btn);
+        email_login = (EditText) findViewById(R.id.main_email);
+        pwd_login = (EditText) findViewById(R.id.main_pwd);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_login.getText().toString().trim();
+                String pwd = pwd_login.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Login!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Login error:(", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+>>>>>>> SignInUpWithoutGoogle
     }
 
     @Override
--- app/src/main/java/net/skhu/signin/signUpActivity.java
+++ app/src/main/java/net/skhu/signin/signUpActivity.java
@@ -1,20 +1,41 @@
 package net.skhu.signin;
 
+<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+=======
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+>>>>>>> SignInUpWithoutGoogle
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+<<<<<<< HEAD
 public class signUpActivity extends AppCompatActivity {
+=======
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signUpActivity extends AppCompatActivity {
+    private EditText email_join;
+    private EditText pwd_join;
+    private Button btn;
+    FirebaseAuth firebaseAuth;
+>>>>>>> SignInUpWithoutGoogle
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+<<<<<<< HEAD
         Button button = (Button) findViewById(R.id.button);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -43,5 +64,38 @@ public class signUpActivity extends AppCompatActivity {
     static boolean isEmptyOrWhiteSpace(String s) {
         if (s == null) return true;
         return s.trim().length() == 0;
+=======
+        email_join = (EditText) findViewById(R.id.main_email);
+        pwd_join = (EditText) findViewById(R.id.main_pwd);
+        btn = (Button) findViewById(R.id.button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_join.getText().toString().trim();
+                String pwd = pwd_join.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(signUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(signUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(signUpActivity.this, "Register error:(", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+
+
+>>>>>>> SignInUpWithoutGoogle
     }
 }
